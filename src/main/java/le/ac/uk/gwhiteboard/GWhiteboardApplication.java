f52ec087a32b00460384d1310fef2534a4d5832e
octane <jubaerhasan1147@gmail.com>
2023-12-11 23:53:18 +0000

    barebone spring project

---
+++ src/main/java/le/ac/uk/gwhiteboard/GWhiteboardApplication.java
@@ -0,0 +1,13 @@
+package le.ac.uk.gwhiteboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GWhiteboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GWhiteboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/le/ac/uk/gwhiteboard/GWhiteboardApplicationTests.java
@@ -0,0 +1,13 @@
+package le.ac.uk.gwhiteboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GWhiteboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
